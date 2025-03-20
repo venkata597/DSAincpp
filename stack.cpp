5b87c89f2b337242f5e2081adb2fae962f5b81a7
Venkata Sai Raghava Regalla <rvsraghava5@gmail.com>
2025-03-21 04:30:42 +0530

    Stack implemented using templates

--- stack.cpp
+++ stack.cpp
@@ -1,22 +1,50 @@
 #include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <string>
 
-template<typename t,int size> class stack{
+template<typename t> class stack{
 private:
-    t stack[size];
+    std::vector<t> stk;
     int stack_pointer = -1;
-public:
-    void push(int val){
-        if(stack_pointer>=size){
-            std::cerr << "Stack Overflow" << std::endl;
+public: 
+    void push(t n){
+        stk.push_back(n);
+        stack_pointer++;
+    }
+    t pop(){
+        if(stack_pointer==-1){
+            std::cout << "Stack is Empty" << std::endl;
         }
         else{
-            stack_pointer++;
-            stack[stack_pointer] = val;
+            t popped = stk.pop_back();
+            stack_pointer--;
+            return popped;
         }
     }
-    void pop(){
-        if(tos==-1){
-            std::cout << "Stack Underflow" << std::endl;
+    void peek() const {
+        if(stack_pointer==-1){
+            std::cout << "Stack is empty" << std::endl;
+        }
+        else{
+            std::cout << "The top element of the stack is: " << stk[stack_pointer] << std::endl;
+        }
+    }
+    void display() const {
+        if(stack_pointer==-1){
+            std::cout << "Stack is empty" << std::endl;
+        }
+        else{
+            std::cout << "-------" << std::endl;
+           for(int i = stack_pointer;i>=0;i--){
+                std::cout << "|  "<< stk[i] <<"  |" << std::endl;
+                std::cout << "-------" << std::endl;
+           }
+           
         }
     }
 };
+
+int main(){
+    stack<int> stack;   
+}
