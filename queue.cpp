fb6d8431bcf7f2371473d1b67c14c50473593fdd
Venkata Sai Raghava Regalla <rvsraghava5@gmail.com>
2025-03-21 21:08:48 +0530

    Queue implemented using arrays and templates

---
+++ queue.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+
+template<typename t,int size> class queue{
+private:    
+    t queuearr[size];
+    int front=-1,rear=-1;
+public:
+    void enqueue(t ele){
+        if(isFull()){
+            std::cout << "Queue Overflow" << std::endl;
+        }
+        else{
+            queuearr[++rear] = ele;
+        }
+        if(front == -1){
+            front = 0;
+        }
+    }
+
+    t dequeue(){
+        if(front == -1){
+            std::cout << "Queue is empty" << std::endl;
+        }
+        else{
+            t dqd = queuearr[front++];
+            return dqd;
+        }
+    }
+
+    void peek(){
+        if(front == -1){
+            std::cout << "Queue is empty" << std::endl;
+        }
+        else{
+            std::cout << "The element at the front: "<<queuearr[front] << std::endl;
+        }
+    }
+
+    bool isEmpty(){
+        if(front == -1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    bool isFull(){
+        if(rear == size){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
